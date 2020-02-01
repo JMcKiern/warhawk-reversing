2dfe7266f1642c14d1b0d8363f3576503fa12d4e
Jack McKiernan <jmckiern@tcd.ie>
2020-02-01 17:12:57 +0000

    Delay cast to bytearray

--- rtt2dds.py
+++ rtt2dds.py
@@ -1,55 +1,44 @@
 import math
 import os
 import sys
+import struct
 
-def get_DXTn_CC(DXTn):
-    if DXTn in range(1,6):
-        return bytearray.fromhex(hex(0x30 + DXTn)[2:])
-    # elif DXTn == 10:
-    else:
-        raise ValueError('Unknown image format')
-
-def create_DDS_pixelformat(DXTn):
+def create_DDS_pixelformat(fourCC):
     '''https://docs.microsoft.com/en-us/windows/win32/direct3ddds/dds-pixelformat
     '''
-    size = bytearray(b'\x20\x00\x00\x00')
-    flags = bytearray(b'\x04\x00\x00\x00') # TODO: dwFlags
-    fourCC = bytearray(b'\x44\x58\x54') + get_DXTn_CC(DXTn)
-    RGBBitCount = bytearray(b'\x00\x00\x00\x00')
-    RBitMask = bytearray(b'\x00\x00\x00\x00')
-    GBitMask = bytearray(b'\x00\x00\x00\x00')
-    BBitMask = bytearray(b'\x00\x00\x00\x00')
-    ABitMask = bytearray(b'\x00\x00\x00\x00')
-    return (size + flags + fourCC + RGBBitCount + RBitMask + GBitMask + BBitMask + ABitMask)
+    size = int32_to_bytes(32)
+    flags =  b'\x04\x00\x00\x00' # TODO: dwFlags
+    # fourCC
+    RGBBitCount = b'\x00\x00\x00\x00' #TODO
+    RBitMask = b'\x00\x00\x00\x00' #TODO
+    GBitMask = b'\x00\x00\x00\x00' #TODO
+    BBitMask = b'\x00\x00\x00\x00' #TODO
+    ABitMask = b'\x00\x00\x00\x00' #TODO
+    return bytearray(size + flags + fourCC + RGBBitCount + RBitMask + GBitMask + BBitMask + ABitMask)
 
-def int_to_32ba_BE(number):
-    output = bytearray()
-    while number > 0:
-        byte = number & 0xFF
-        output += bytearray.fromhex('{0:02x}'.format(byte))
-        number >>= 8
-    return output.ljust(4, b'\x00')
+def int32_to_bytes(number):
+    return struct.pack("<I", number)
 
-def create_DDS_header(DXTn, res_width, res_height):
+def create_DDS_header(fourCC, res_width, res_height):
     '''https://docs.microsoft.com/en-us/windows/win32/direct3ddds/dds-header
     '''
-    dds_magic = bytearray(b'\x44\x44\x53\x20')
-    header_size = bytearray(b'\x7C\x00\x00\x00') # dwSize
-    flags = bytearray(b'\x07\x10\x00\x00') # TODO: dwFlags
-    height = int_to_32ba_BE(res_height)
-    width = int_to_32ba_BE(res_width)
-    pitch_or_linear_size = bytearray(b'\x00\x00\x00\x00') # TODO
-    depth = bytearray(b'\x00\x00\x00\x00') # TODO
-    mipmapcount = bytearray(b'\x00\x00\x00\x00') # TODO
-    reserved = bytearray(b'\x00\x00\x00\x00') * 11 # TODO
-    ddspf = create_DDS_pixelformat(DXTn)
-    caps = bytearray(b'\x08\x10\x40\x00') # TODO
-    caps2 = bytearray(b'\x00\x00\x00\x00') # TODO
-    caps3 = bytearray(b'\x00\x00\x00\x00')
-    caps4 = bytearray(b'\x00\x00\x00\x00')
-    reserved2 = bytearray(b'\x00\x00\x00\x00')
+    dds_magic = b'DDS '
+    header_size = int32_to_bytes(124) #dwSize
+    flags = b'\x07\x10\x00\x00' # TODO: dwFlags
+    height = int32_to_bytes(res_height)
+    width = int32_to_bytes(res_width)
+    pitch_or_linear_size = b'\x00\x00\x00\x00' # TODO
+    depth = b'\x00\x00\x00\x00' # TODO
+    mipmapcount = b'\x00\x00\x00\x00' # TODO
+    reserved = b'\x00\x00\x00\x00' * 11
+    ddspf = create_DDS_pixelformat(fourCC)
+    caps = b'\x08\x10\x40\x00' # TODO
+    caps2 = b'\x00\x00\x00\x00' # TODO
+    caps3 = b'\x00\x00\x00\x00'
+    caps4 = b'\x00\x00\x00\x00'
+    reserved2 = b'\x00\x00\x00\x00'
 
-    return (dds_magic +
+    return bytearray(dds_magic +
     header_size +
     flags +
     height +
@@ -64,7 +53,6 @@ def create_DDS_header(DXTn, res_width, res_height):
     caps3 +
     caps4 +
     reserved2)
-
 
     # DWORD           dwSize;
     # DWORD           dwFlags;
@@ -90,13 +78,15 @@ def rtt2dds(filepath):
     if data[0x0] != 0x80:
         raise ValueError('Expecting 0x80 at pos 0')
 
-    # Find DXTn format
-    if data[0x4] == 0x06:
-        DXTn = 1
+    # Find compression method
+    if data[0x4] == 0x05: # No compression
+        fourCC = b'\x00\x00\x00\x00'
+    elif data[0x4] == 0x06:
+        fourCC = b'DXT1'
     elif data[0x4] == 0x07:
-        DXTn = 3
+        fourCC = b'DXT3'
     elif data[0x4] == 0x08:
-        DXTn = 5
+        fourCC = b'DXT5'
     else:
         raise ValueError('Unknown image format')
 
@@ -105,7 +95,7 @@ def rtt2dds(filepath):
     width = (data[0x8] << 8) + data[0x9]
     height = (data[0xa] << 8) + data[0xb]
 
-    DDS_header = create_DDS_header(DXTn, width, height)
+    DDS_header = create_DDS_header(fourCC, width, height)
     for i in range(0, len(DDS_header)):
         data[i] = DDS_header[i]
 
