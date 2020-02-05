ff779358e596f8abd0183dd2dfe99da9836f83a0
Jack McKiernan <jmckiern@tcd.ie>
2020-02-05 21:24:04 +0000

    Remove DDS header info - it's apparent from code

--- rtt2dds.py
+++ rtt2dds.py
@@ -128,21 +128,6 @@ def create_DDS_header(fourCC, width, height, num_mipmaps, hasAlpha, isPitch,
     caps4 +
     reserved2)
 
-    # DWORD           dwSize;
-    # DWORD           dwFlags;
-    # DWORD           dwHeight;
-    # DWORD           dwWidth;
-    # DWORD           dwPitchOrLinearSize;
-    # DWORD           dwDepth;
-    # DWORD           dwMipMapCount;
-    # DWORD           dwReserved1[11];
-    # DDS_PIXELFORMAT ddspf;
-    # DWORD           dwCaps;
-    # DWORD           dwCaps2;
-    # DWORD           dwCaps3;
-    # DWORD           dwCaps4;
-    # DWORD           dwReserved2;
-
 def rtt2dds(filepath):
     with open(filepath, 'rb') as f:
         data = bytearray(f.read())
